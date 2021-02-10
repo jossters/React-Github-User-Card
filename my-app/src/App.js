42ea072eff461ef2960617084016e09b06da782c
Johnny Murillo <jossters@gmail.com>
2021-02-09 16:21:28 -0800

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './Octocat.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Jossters
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/jossters"
+          target="_blank"
+          rel="Jossters"
+        >
+          🤑User Card🤑
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
